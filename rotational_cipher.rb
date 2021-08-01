d922fec36863d9cba9bff3db6b3f064a4396cbb8
Samar Mishra <samar-mishra@users.noreply.github.com>
2021-08-01 20:11:52 +0530

    Update rotational_cipher.rb

--- rotational_cipher.rb
+++ rotational_cipher.rb
@@ -2,7 +2,7 @@ class RotationalCipher
 
   def rotational_cipher(input, rotation_factor)  
     change_input_to_ascii = input.chars.map{ |c| c.ord }
-    increase_by_rotation_factor = change_to_ascii.map{ |i| i+rotation_factor }
-    cipher_string = increase_by_rotation_factor.map{ |x| x.chr }.join
+    increament_by_rotation_factor = change_input_to_ascii.map{ |i| i+rotation_factor }
+    ciphered_string = increament_by_rotation_factor.map{ |x| x.chr }.join
   end  
 end
